7b916a6838f6ee5425397930b7231e3ed5f315e3
Ben Rosner <ben.rosner.williamsburg@gmail.com>
2023-08-23 16:28:35 -0400

    Complete meal tests

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,49 @@
-function nameMenuItem() {
-
+function nameMenuItem(name) {
+ return `Delicious ${name}`
 }
 
+function createMenuItem(name, price, type){
+  var menuItem = {
+    name: name,
+    price: price,
+    type: type
+  }
+  return menuItem;
+}
+
+function addIngredients(ingredient, ingredients) {
+  if (!ingredients.includes(ingredient)){
+    ingredients.push(ingredient);
+  }
+  return ingredients;
+}
+
+function formatPrice(initialPrice){
+  return `$${initialPrice}`;
+}
+
+function decreasePrice(price){
+    var discount = price / 10;
+    var newPrice = price - discount;
+    return newPrice;
+}
+
+function createRecipe(title, ingredients, type){
+  var recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: type
+  }
+  return recipe;
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
 
 
