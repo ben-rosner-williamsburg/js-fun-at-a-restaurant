523735e0d82f141e73e919fe0d8d8f3fa94ccdbc
Ben Rosner <ben.rosner.williamsburg@gmail.com>
2023-08-25 11:17:48 -0400

    Complete order tests

--- src/order.js
+++ src/order.js
@@ -1,7 +1,42 @@
+function takeOrder(order, deliveryOrders) {
+  if (deliveryOrders.length <= 2) {
+    deliveryOrders.push(order);
+  }
+  return deliveryOrders;
+}
+
+
+function refundOrder(orderNumber, deliveryOrders) {
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (orderNumber === deliveryOrders[i].orderNumber) {
+      deliveryOrders.splice(i, 1);
+    }
+  }
+  return deliveryOrders;
+}
+
+function listItems(orders) {
+  var orderNames = [];
+  for (var i = 0; i < orders.length; i++) {
+    orderNames.push(orders[i].item);
+  }
+  orderNames = orderNames.toString().replaceAll(",", ", ");
+  return orderNames;
+}
+
+function searchOrder(orders, itemName) {
+  var searchResult = false;
+  for (var i = 0; i < orders.length; i++) {
+    if (orders[i].item === itemName) {
+      searchResult = true;
+    }
+  }
+  return searchResult;
+}
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
